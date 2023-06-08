06a821c557a7d956d65d5ab53065bbd20f214b02
310-1 <310-1@310-11>
2023-06-08 15:22:58 +0900

    .

--- zblog/src/main/java/com/sg/leo/controller/UserController.java
+++ zblog/src/main/java/com/sg/leo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.sg.leo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +14,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sg.leo.domain.RoleType;
 import com.sg.leo.domain.User;
+import com.sg.leo.dto.ResponseDTO;
 import com.sg.leo.exception.ZblogException;
 import com.sg.leo.repository.UserRepository;
+import com.sg.leo.service.UserService;
 
 @Controller
 public class UserController {
@@ -51,5 +54,26 @@ public class UserController {
 	@GetMapping("/auth/insertUser")
 	public String insertUser() {
 		return "user/insertUser";
+	}
+	
+	@Autowired
+	private UserService userSerice;
+	@PostMapping("/auth/insertUser")
+	public @ResponseBody ResponseDTO<?> insertUsers(@RequestBody User user){
+		
+		User findUser = userSerice.getUser(user.getUsername());
+		if(findUser.getUsername() == null) {
+			// 같은 username회원 없음, 회원가입 진행
+			userSerice.insertUser(user);
+			return new ResponseDTO<>(HttpStatus.OK.value(), 
+					user.getUsername() + "님 회원가입 성공했어요");
+		}else {
+			
+			
+			// 중복된 username있음, 에러메시지 표시
+			return new ResponseDTO<>(HttpStatus.BAD_REQUEST.value(), 
+					user.getUsername() + "님 이미 회원임 또는 username 사용중");
+		}
+		
 	}
 }
