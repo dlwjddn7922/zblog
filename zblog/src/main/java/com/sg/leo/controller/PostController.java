7352fb6d34d56477af0df4319f114ef619d7ac13
310-1 <310-1@310-11>
2023-06-15 14:32:00 +0900

    .

--- zblog/src/main/java/com/sg/leo/controller/PostController.java
+++ zblog/src/main/java/com/sg/leo/controller/PostController.java
@@ -3,7 +3,9 @@ package com.sg.leo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -37,5 +39,12 @@ public class PostController {
 		post.setUser(blogprincipal);
 		postService.insertPost(post);
 		return new ResponseDTO<>(HttpStatus.OK.value(), "새로운포스트 등록");
+	}
+	
+	@GetMapping("/post/{id}")
+	public String getPost( @PathVariable int id, Model model)
+	{
+		model.addAttribute("thepost", postService.getPost(id));
+		return "post/getPost";
 	}
 }
